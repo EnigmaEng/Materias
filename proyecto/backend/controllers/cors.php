387a1116166ec4d9e02daff372009f352deb2c76
lucioschiavoni <luciosc1798@gmail.com>
2023-09-10 14:06:52 -0300

    se actualiza el backend

--- proyecto/backend/controllers/cors.php
+++ proyecto/backend/controllers/cors.php
@@ -3,5 +3,4 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 header("content-type: application/json; charset=utf-8");
-
 ?>
