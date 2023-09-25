0b4e5a99d3d0f15dfd61e2b731bfaeafb2af2b28
Maxi Dom <maxidom99@gmail.com>
2023-09-25 19:32:22 -0300

    Quitando ignore

---
+++ proyecto/frontend/cypress/e2e/02-Restaurante/RegistroRestaurante.cy.js
@@ -0,0 +1,19 @@
+describe ('Registro_Rest', () => {
+    beforeEach (() => { 
+        cy.visit('http://127.0.0.1:5173/')
+    })
+    it('Entrando al Registro',() =>{
+        cy.contains('Where We Eat')
+        cy.get('[href="/registro"] > .w-60').click()
+        cy.get('#alias').type('Test_UserR')
+        cy.get('#email').type('RestauranteEmail@gmail.com')
+        cy.get('#contrasena').type('SoyRestaurante123')
+        cy.get('#confirmContrasena').type('SoyRestaurante123')
+        cy.get('#url_img_usuario').type('Foto_Resto.jpg')
+        cy.get('#rol').select('Restaurante')
+        cy.get('#nombre').type('Resto Cypress')
+        cy.get(':nth-child(10) > input').click()
+        cy.get('.mb-8').click()
+        cy.contains('Registro exitoso')
+    })
+})
