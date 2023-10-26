a7624dfa4e53344bba2e8a6c51fd0081ea9a5393
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2023-10-25 23:25:47 -0300

    Style: Boton para ir hacia atras

--- proyecto/frontend/src/pages/user/perfil/restaurante/EditarPerfi.jsx
+++ proyecto/frontend/src/pages/user/perfil/restaurante/EditarPerfi.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import NavBar from '../../../../components/nabvar/NavBar'
 import { Link } from 'react-router-dom'
+import {BiArrowBack} from 'react-icons/bi'
 const EditarPerfi = () => {
   return (
   <>
  
     <div className='min-h-screen text-lg text-wwe   dark:bg-zinc-800 dark:bg-opacity-95   '>
        <NavBar/>
-       <Link to='/configuracionRest' className='absolute top-24 left-24 bg-wwe text-white rounded-lg px-6 py-1'>Volver</Link> 
+        <Link to='/configuracionRest' className='absolute  md:left-24 left-4 top-20 bg-wwe text-white rounded-lg px-6 py-2'>  <BiArrowBack/> </Link>
   <h3 className='text-5xl text-center mt-24'>Editar perfil formulario</h3>
     </div>
     </>
