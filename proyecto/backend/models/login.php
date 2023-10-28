f843859ececff29a6805d32ac3eaf02fd9ae01d4
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2023-10-27 22:12:10 -0300

    "Fix: se mergean los cambios"

--- proyecto/backend/models/login.php
+++ proyecto/backend/models/login.php
@@ -66,7 +66,7 @@ class Login extends DataBaseConnection
                 $stmt->execute();
                 $rolData = $stmt->fetch(PDO::FETCH_ASSOC);
             } elseif ($rol === 'A') {
-                //Consulta para obtener datos de admin según el usuario con rol 'A'
+                //Consulta para obtener datos de admin según el usuario con rol 'A'.
                 $query = "SELECT * FROM administrativo WHERE id_usuario = :id_usuario";
                 $stmt = $conn->prepare($query);
                 $stmt->bindValue(":id_usuario", $userData['id_usuario']);
