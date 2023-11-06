26b5d5d141e0015a90add60d25dcd01bff707075
Maxi Dom <maxidom99@gmail.com>
2023-11-05 21:46:53 -0300

    login

--- proyecto/frontend/cypress/e2e/02-Restaurante/EditarPerfilRest.cy.js
+++ proyecto/frontend/cypress/e2e/02-Restaurante/EditarPerfilRest.cy.js
@@ -1,17 +1,16 @@
 describe('LoginRestaurante', () => {
     beforeEach(() => {
-      cy.visit('http://wweat.ddns.net/')
+      cy.visit('http://127.0.0.1:5173/')
     })
   
     it('Editar Perfil Rest', () => {
       cy.get('.hidden > :nth-child(1) > .z-10 > .gap-5 > [href="/login"] > .bg-wwe').click()
       cy.contains('Iniciar Sesión')
       cy.get('#email').type('RestauranteEmail@gmail.com', { delay: 25 })
-      cy.get('#contrasena').type('SoyRestaurante123', { delay: 25 })
+      cy.get('#contrasena').type('hola1234', { delay: 25 })
       cy.get(':nth-child(3) > .flex').click()
       cy.contains('Menú')
-      cy.wait(1000);
-      cy.get('#cypress').click({ force: true });
+      cy.get('#cypress').click();
       cy.contains('Configuración').click();
       cy.get(':nth-child(2) > .flex > .font-aref').click();
       cy.get('#alias').type('Test_User_R');
@@ -19,7 +18,7 @@ describe('LoginRestaurante', () => {
       cy.get('#url_img_usuario').selectFile("./cypress/imgs/rest_cypress.jpg");
       cy.get('.md\\:w-64').click();
       cy.contains('Los cambios persistieron correctamente.');
-      /*cy.wait(1500);
+      cy.wait(3000);
       cy.get('#cypress').click();
       cy.contains('Salir').click()
       cy.get('.m-auto > .w-14').click()
@@ -28,6 +27,6 @@ describe('LoginRestaurante', () => {
       cy.get('#contrasena').type('SoyRestaurante123', { delay: 25 })
       cy.get(':nth-child(3) > .flex').click()
       cy.get('#cypress').click();
-      cy.contains('Perfil').click();*/
+      cy.contains('Perfil').click();
     })
   })
