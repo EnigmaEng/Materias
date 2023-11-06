02eb648adc0dc2ab64ad4a5611b0f19643504382
Maxi Dom <maxidom99@gmail.com>
2023-11-05 21:51:06 -0300

    Arreglos Login

--- proyecto/backend/models/login.php
+++ proyecto/backend/models/login.php
@@ -58,7 +58,7 @@ class Login
                 $rolData = $stmt->fetch(PDO::FETCH_ASSOC);
             } elseif ($rol === 'T') {
                 
-                $query = "SELECT * FROM turista WHERE tur.id_usuario = :id_usuario";
+                $query = "SELECT * FROM turista tur WHERE tur.id_usuario = :id_usuario";
                 $stmt = $conn->prepare($query);
                 $stmt->bindValue(":id_usuario", $userData['id_usuario']);
                 $stmt->execute();
