057c344f93ad235458cff25a1ae838f144fa4a3e
Maxi Dom <maxidom99@gmail.com>
2023-11-06 20:43:35 -0300

    Cypress

--- proyecto/frontend/cypress/e2e/02-Restaurante/VerDescuentos.cy.js
+++ proyecto/frontend/cypress/e2e/02-Restaurante/VerDescuentos.cy.js
@@ -4,13 +4,14 @@ describe ('LoginRestaurante', () => {
         //cy.visit('http://127.0.0.1:5173/')
     })
 
-    it('Ver platos  ',() =>{
+    it('Crear descuento',() =>{
         cy.get('.hidden > :nth-child(1) > .z-10 > .gap-5 > [href="/login"] > .bg-wwe').click()
         cy.contains('Iniciar Sesión')
         cy.get('#email').type('RestauranteEmail@gmail.com',{ delay: 15 })
         cy.get('#contrasena').type('SoyRestaurante123',{ delay: 15 })
         cy.get(':nth-child(3) > .flex').click()
-        cy.contains('Ver menú').click()
-        cy.wait(15000)
+        cy.get('#cypress').click();
+        cy.contains('Configuración').click();
+        cy.get(':nth-child(4) > .flex > .font-aref').click()
     })
 })
